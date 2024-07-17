f565d5afb6561e100f9d9e40489a17636722ac7f
T-Luxshan <luckybraveboys@gmail.com>
2024-07-17 15:45:16 +0530

    route updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Payment from './components/Payment';
 import Home from './pages/Home';
+import Success from './components/Success';
+import Failure from './components/Failure';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="payment" element={<Payment />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/failure" element={<Failure />} />
       </Routes>
     </BrowserRouter>
   );
