3f29a3fb1032b3f4d8b8abb2be9e2bbd1791d876
T-Luxshan <luckybraveboys@gmail.com>
2024-07-18 17:18:05 +0530

    endpoint path modified

--- src/service/PaymentService.js
+++ src/service/PaymentService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const initiatePayment = (name, email, amount) =>{
-    return axios.post(`${process.env.REACT_APP_BASE_URL}/checkout`,{
+    return axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/payment`,{
         name, email, amount
     })
 }
